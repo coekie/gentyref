66421d1cab0b849ad1ce73a1631dd361f38ba092
Wouter Coekaerts <wouter@coekaerts.be>
2009-01-10 19:26:43 +0000

    Refactor a bit to make it easier to apply test to another implementation that has a getExactSuperType implementation
    git-svn-id: https://gentyref.googlecode.com/svn/trunk@27 9a24f7ba-d458-11dd-86d7-074df07e0730

--- src/test/java/com/googlecode/gentyref/GenTyRefReflectionStrategy.java
+++ src/test/java/com/googlecode/gentyref/GenTyRefReflectionStrategy.java
@@ -1,32 +1,15 @@
-/**
- * 
- */
 package com.googlecode.gentyref;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
-
-class GenTyRefReflectionStrategy implements ReflectionStrategy {
-	public void testExactSuperclass(Type expectedSuperclass, Type type) {
-		// test if the supertype of the given class is as expected
-		GenericTypeReflectorTest.assertEquals(expectedSuperclass, getExactSuperType(type, GenericTypeReflectorTest.getClassType(expectedSuperclass)));
-	}
-
+class GenTyRefReflectionStrategy extends AbstractReflectionStrategy {
 	public boolean isSupertype(Type superType, Type subType) {
 		return GenericTypeReflector.isSuperType(superType, subType);
 	}
 
-	public void testInexactSupertype(Type superType, Type subType) {
-		if (superType instanceof ParameterizedType || superType instanceof Class) {
-			// test if it's not exact
-			GenericTypeReflectorTest.assertFalse(superType.equals(getExactSuperType(subType, GenericTypeReflectorTest.getClassType(superType))));
-		}
-	}
-
-	private Type getExactSuperType(Type type, Class<?> searchClass) {
+	protected Type getExactSuperType(Type type, Class<?> searchClass) {
 		return GenericTypeReflector.getExactSuperType(type, searchClass);
 	}
 
