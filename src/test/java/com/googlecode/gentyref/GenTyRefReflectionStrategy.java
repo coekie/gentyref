2b44d5d60d50bd5b3664644cb88fc24d86a193da
Wouter Coekaerts <wouter@coekaerts.be>
2010-02-10 20:55:51 +0000

    Add sanity check that new description of getSuperType as suggested in Issue #8 is indeed how it behaves
    git-svn-id: https://gentyref.googlecode.com/svn/trunk@52 9a24f7ba-d458-11dd-86d7-074df07e0730

--- src/test/java/com/googlecode/gentyref/GenTyRefReflectionStrategy.java
+++ src/test/java/com/googlecode/gentyref/GenTyRefReflectionStrategy.java
@@ -4,12 +4,22 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 
+import junit.framework.Assert;
+
 class GenTyRefReflectionStrategy extends AbstractReflectionStrategy {
 	public boolean isSupertype(Type superType, Type subType) {
 		return GenericTypeReflector.isSuperType(superType, subType);
 	}
 
 	protected Type getExactSuperType(Type type, Class<?> searchClass) {
+		Type result = GenericTypeReflector.getExactSuperType(type, searchClass);
+		
+		// sanity check: erase(result) == searchClass and result is a supertype of type 
+		if (result != null) {
+			Assert.assertEquals(searchClass, GenericTypeReflector.erase(result));
+			GenericTypeReflector.isSuperType(result, type);
+		}
+		
 		return GenericTypeReflector.getExactSuperType(type, searchClass);
 	}
 
